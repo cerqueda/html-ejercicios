16d6461b543e4fe11a892528818a6e29a8bcb24a
Cecilia Cerqueda <pzcerqueda@gmail.com>
2017-03-16 10:23:47 -0600

    celsius actualizado

--- Assets/Far-Celsius/FCelsius.js
+++ Assets/Far-Celsius/FCelsius.js
@@ -1,12 +1,13 @@
-function convertirCelsius(){
-  var fTempVal = parseFloat(document.getElementById('fTemp').value);
-var cTempVal = (fTempVal - 32) * (5/9);
-document.getElementById('cTemp').value = cTempVal;
+function convertirCelsius(farenheit){
+  var fTempVal = parseFloat(document.getElementById('farenheit').value);
+  var celsius = document.getElementById('celsius');
+  var cTempVal = (fTempVal - 32) * (5/9);
+  celsius.value = cTempVal;
+
 }
 function convertToF(celsius) {
     var cTempVal = parseFloat(document.getElementById('celsius').value);
+    var farenheit = document.getElementById('farenheit');
     var fTempVal = (cTempVal * (9/5)) + 32;
-    document.getElementById('fTemp').value = fTempVal;
+    farenheit.value=fTempVal;
 }
-
-document.getElementById('farenheit').value
